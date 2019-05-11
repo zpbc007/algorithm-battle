38286860916fcc3280b6287c92a9b0d60e254266
zhaopeng <719197352@qq.com>
2019-05-11 11:46:19 +0800

    counting sort work

---
+++ sort-algorithm/counting-sort/__tests__/get-max.test.ts
@@ -0,0 +1,23 @@
+import { getMinAndMax } from "../get-max-min";
+
+describe('get max', () => {
+    it('should get minVal and maxVal in arr', () => {
+        const arr = [1, 2, 3, 5, 6, 9, 0]
+        const result = getMinAndMax(arr)
+
+        expect(result).toEqual({
+            max: 9,
+            min: 0
+        })
+    })
+
+    it('should get null when arr is empty', () => {
+        const arr = []
+        const reuslt = getMinAndMax(arr)
+
+        expect(reuslt).toEqual({
+            max: null,
+            min: null
+        })
+    })
+})
