ca84e257aa0a50d2bac9e4c17c88dac12ed2d316
zhaopeng <719197352@qq.com>
2019-05-11 13:51:36 +0800

    bucket work

---
+++ sort-algorithm/bucket-sort/index.ts
@@ -0,0 +1,28 @@
+import { getMinAndMax } from "@utils/get-max-min";
+import { quickSort } from "@algorithm/quick-sort";
+
+export function bucketSort(arr: number[], bucketSize = 5) {
+    const { max, min } = getMinAndMax(arr)
+
+    // 桶的数量
+    const bucketCount = Math.floor((max -min) / bucketSize) + 1
+    const buckets: number[][] = []
+    for (let i = 0; i < bucketCount; i++) {
+        buckets[i] = []
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        buckets[Math.floor((arr[i] - min) / bucketSize)].push(arr[i])
+    }
+
+    arr= []
+    buckets.map((item, index) => {
+        quickSort(item)
+
+        item.map((value) => {
+            arr.push(value)
+        })
+    })
+
+    return arr
+}
