0b52af52363dd13b2e1b5fdf98154e02edc57a01
zhaopeng <719197352@qq.com>
2019-05-16 21:22:05 +0800

    add insert sort

---
+++ sort-algorithm/insert-sort/index.ts
@@ -0,0 +1,12 @@
+export function inserSort(arr: number[]) {
+    for (let i = 1; i < arr.length; i++) {
+        let preIndex = i - 1
+        const current = arr[i]
+        while(preIndex >= 0 && arr[preIndex] > current) {
+            arr[preIndex + 1] = arr[preIndex]
+            preIndex--
+        }
+
+        arr[preIndex+1] = current
+    }
+}
