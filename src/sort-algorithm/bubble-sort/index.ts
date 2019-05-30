794115b602f5666e86d8adc77b34276371436eb8
zhaopeng <719197352@qq.com>
2019-05-30 16:39:29 +0800

    冒泡 注释

--- src/sort-algorithm/bubble-sort/index.ts
+++ src/sort-algorithm/bubble-sort/index.ts
@@ -1,11 +1,17 @@
 import { swap } from 'src/utils/swap'
 
+/**
+ * 冒泡排序
+ * 两层循环
+ * 外层用于遍历整个数组 每次循环会有一个元素（最大、最小）被确定
+ * 内层用于将本次的的最大、最小筛选出来
+ */
 export function bubbleSort(arr: number[]) {
     const len = arr.length
-    for (let i = len - 1; i >= 0; i--) {
-        for (let j = len - 1; j >= len - i; j--) {
+    for (let i = 0; i < len; i++) {
+        for (let j = 1; j < len - i; j++) {
             if (arr[j] < arr[j - 1]) {
-                swap(arr, j - 1, j)
+                swap(arr, j, j - 1)
             }
         }
     }
--- src/utils/swap.ts
+++ src/utils/swap.ts
@@ -1,7 +1,11 @@
-export function swap<T>(arr: T[], i: number, j: number) {
+/**
+ * 交换数组中 两节点
+ */
+export function swap<T = number>(arr: T[], i: number, j: number) {
     if (i === j) {
         return
     }
+
     const temp = arr[i]
     arr[i] = arr[j]
     arr[j] = temp
