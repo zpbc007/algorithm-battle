438d5023625d541cf142e60d0fcb6b59c2768c87
zhaopeng <719197352@qq.com>
2019-05-30 17:30:59 +0800

    插入排序 注释

--- src/sort-algorithm/insert-sort/index.ts
+++ src/sort-algorithm/insert-sort/index.ts
@@ -1,3 +1,8 @@
+/**
+ * 插入排序
+ * 外层遍历从1开始 i之前的视为已排序的
+ * 将 i 插到它应该在的位置上
+ */
 export function insertSort(arr: number[]) {
     for (let i = 1; i < arr.length; i++) {
         let preIndex = i - 1
@@ -9,4 +14,6 @@ export function insertSort(arr: number[]) {
 
         arr[preIndex + 1] = current
     }
+
+    return arr
 }
