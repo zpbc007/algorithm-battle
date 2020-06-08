88fdfa6105d10d88d0f5d3fb685a3c254f87d9cc
赵鹏 <zhapengchrdw@gmail.com>
2020-06-08 23:46:22 +0800

    ADD: 394

---
+++ src/题/394.字符串解码/index.ts
@@ -0,0 +1,42 @@
+export function decodeString(s: string) {
+    const stack = []
+
+    for (const item of s) {
+        if (item === ']') { // 出栈
+            stack.push(getStrAndNumBefore(stack))
+        } else { // 入栈
+            stack.push(item)
+        }
+    }
+
+    return stack.reduceRight((result, str) => str + result, '')
+}
+
+function getStrAndNumBefore(stack: string[]) {
+    let str = ''
+    let numStr = ''
+    let recordNum = false
+
+    while (stack.length > 0) {
+        const item = stack.pop()
+
+        // 开始记录数字
+        if (!recordNum && item === '[') {
+            recordNum = true
+            continue
+        }
+
+        if (recordNum) {
+            // 遇到非数字
+            if (!/[0-9]/.test(item)) {
+                stack.push(item)
+                break
+            }
+            numStr = `${item}${numStr}`
+        } else {
+            str = `${item}${str}`
+        }
+    }
+
+    return str.repeat(Number(numStr))
+}
