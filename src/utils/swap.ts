d63923dd49ad1fe410e9137fde8e47daa3bcc49d
zhaopeng06 <zhaopeng06@kuaishou.com>
2022-01-05 00:18:28 +0800

    build: 添加注释

--- src/utils/swap.ts
+++ src/utils/swap.ts
@@ -7,6 +7,14 @@ export function swap<T>(arr: T[], sourceIndex: number, targetIndex: number) {
     return arr;
 }
 
+/**
+ * 使用异或交换位置（只对数字有效，不然还得做类型转换）
+ * a = 甲 , b = 乙
+ * a = a ^ b => a = 甲 ^ 乙，b = 乙
+ * b = a ^ b => b = 甲 ^ 乙 ^ 乙 = 甲
+ * a = a ^ b => a = 甲 ^ 乙 ^ 甲 => 乙
+ * @returns
+ */
 export function swapByXOR<T>(
     arr: number[],
     sourceIndex: number,
