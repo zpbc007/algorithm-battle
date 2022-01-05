14a294a11934f9e24eb61888ce8555d968a86aa2
zhaopeng06 <zhaopeng06@kuaishou.com>
2022-01-05 22:29:41 +0800

    doc

--- src/sort/insert.ts
+++ src/sort/insert.ts
@@ -3,6 +3,7 @@ import { swapByXOR } from '..';
 /**
  * 插入排序
  * 左侧为已经有序部分，不断扩大有序部分的范围
+ * 虽然也是 O(n^2)的算法，但在数组有序的情况下表现更好
  */
 export function insertSort(arr: number[]) {
     for (let i = 1; i < arr.length; i++) {
